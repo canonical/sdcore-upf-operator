a45efebcf4388832dfe32c212b54b633852cabe1
Guillaume Belanger <guillaume.belanger@canonical.com>
2024-02-07 18:17:42 -0500

    feat: Add sdcore-upf snap installation (#3)

--- src/charm.py
+++ src/charm.py
@@ -7,20 +7,51 @@
 import logging
 
 import ops
+from charms.operator_libs_linux.v2 import snap
+from ops.model import ActiveStatus, BlockedStatus
+
+UPF_SNAP_NAME = "sdcore-upf"
+UPF_SNAP_CHANNEL = "latest/edge"
+UPF_SNAP_REVISION = "3"
+
 
 logger = logging.getLogger(__name__)
 
 
-class SdcoreUpfOperatorCharm(ops.CharmBase):
+class SdcoreUpfCharm(ops.CharmBase):
     """Machine charm for SD-Core User Plane Function."""
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.install, self._configure)
+        self.framework.observe(self.on.update_status, self._configure)
+        self.framework.observe(self.on.config_changed, self._configure)
 
-    def _on_config_changed(self, event):
-        self.unit.status = ops.ActiveStatus()
+    def _configure(self, _):
+        """Handle UPF installation."""
+        if not self.unit.is_leader():
+            self.unit.status = BlockedStatus("Scaling is not implemented for this charm")
+            return
+        self._install_upf_snap()
+        self.unit.status = ActiveStatus()
+
+    def _install_upf_snap(self) -> None:
+        """Installs the UPF snap in the machine."""
+        try:
+            snap_cache = snap.SnapCache()
+            upf_snap = snap_cache[UPF_SNAP_NAME]
+            upf_snap.ensure(
+                snap.SnapState.Latest,
+                channel=UPF_SNAP_CHANNEL,
+                revision=UPF_SNAP_REVISION,
+                devmode=True,
+            )
+            upf_snap.hold()
+            logger.info("UPF snap installed")
+        except snap.SnapError as e:
+            logger.error("An exception occurred when installing the UPF snap. Reason: %s", str(e))
+            raise e
 
 
 if __name__ == "__main__":  # pragma: nocover
-    ops.main(SdcoreUpfOperatorCharm)  # type: ignore
+    ops.main(SdcoreUpfCharm)  # type: ignore
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -2,19 +2,87 @@
 # See LICENSE file for licensing details.
 
 import unittest
+from typing import List, Optional
+from unittest.mock import patch
 
 import ops
 import ops.testing
-from charm import SdcoreUpfOperatorCharm
+from charm import SdcoreUpfCharm
+from charms.operator_libs_linux.v2.snap import SnapState
+
+
+class MockSnapObject:
+    def __init__(self, name):
+        self.name = name
+        self.ensure_called = False
+        self.ensure_called_with = None
+        self.hold_called = False
+
+    def ensure(
+        self,
+        state,
+        classic: Optional[bool] = False,
+        devmode: Optional[bool] = False,
+        channel: Optional[str] = "",
+        cohort: Optional[str] = "",
+        revision: Optional[str] = None,
+    ):
+        self.ensure_called = True
+        self.ensure_called_with = (state, classic, devmode, channel, cohort, revision)
+
+    def hold(self):
+        self.hold_called = True
+
+    def start(self, services: Optional[List[str]] = None, enable: Optional[bool] = False) -> None:
+        self.start_called = True
+        self.start_called_with = {"services": services, "enable": enable}
 
 
 class TestCharm(unittest.TestCase):
     def setUp(self):
-        self.harness = ops.testing.Harness(SdcoreUpfOperatorCharm)
+        self.harness = ops.testing.Harness(SdcoreUpfCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_given_when_config_changed_then(self):
+    def test_given_unit_is_not_leader_when_config_changed_then_status_is_blocked(self):
+        self.harness.set_leader(False)
+
+        self.harness.update_config()
+
+        self.assertEqual(
+            self.harness.model.unit.status,
+            ops.BlockedStatus("Scaling is not implemented for this charm"),
+        )
+
+    @patch("charms.operator_libs_linux.v2.snap.SnapCache")
+    def test_given_upf_snap_uninstalled_when_configure_then_upf_snap_installed(
+        self, mock_snap_cache
+    ):
+        self.harness.set_leader(is_leader=True)
+        upf_snap = MockSnapObject("upf")
+        snap_cache = {"sdcore-upf": upf_snap}
+        mock_snap_cache.return_value = snap_cache
+
+        self.harness.charm.on.install.emit()
+
+        mock_snap_cache.assert_called_with()
+        assert upf_snap.ensure_called
+        assert upf_snap.ensure_called_with == (
+            SnapState.Latest,
+            False,
+            True,
+            "latest/edge",
+            "",
+            "3",
+        )
+        assert upf_snap.hold_called
+
+    @patch("charms.operator_libs_linux.v2.snap.SnapCache")
+    def test_given_unit_is_leader_when_config_changed_then_status_is_active(self, mock_snap_cache):
+        self.harness.set_leader(True)
+        upf_snap = MockSnapObject("sdcore-upf")
+        snap_cache = {"sdcore-upf": upf_snap}
+        mock_snap_cache.return_value = snap_cache
 
         self.harness.update_config()
 
