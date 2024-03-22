07f2a6ec2b1d79d08ccdeeebbd79c9bc3a672b90
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2024-03-22 11:40:55 +0100

    Adding tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,12 +5,13 @@
 import asyncio
 import json
 import logging
+import os
 import re
 from pathlib import Path
 
 import pytest
 import yaml
-from charm import PFCP_PORT
+from charm import PFCP_PORT, UPF_CONFIG_FILE_NAME, UPF_CONFIG_PATH
 from juju.application import Application
 from juju.machine import Machine
 from juju.model import Model
@@ -21,118 +22,199 @@ logger = logging.getLogger(__name__)
 METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
 MODEL_NAME = "upf-integration"
-UPF_CONFIG_FILE_PATH = "/var/snap/sdcore-upf/common/upf.json"
+ACCESS_INTERFACE_NAME = "enp6s0"
+CORE_INTERFACE_NAME = "enp7s0"
 
 
-@pytest.mark.abort_on_fail
-async def test_given_upf_machine_charm_built_when_deploy_than_charm_goes_to_active_status(
-    ops_test: OpsTest
-):
-    charm = await ops_test.build_charm(".")
-    await ops_test.model.connect(model_name=MODEL_NAME)
+class TestUPFMachineCharm:
+    @pytest.fixture(autouse=True)
+    @pytest.mark.abort_on_fail
+    async def setup(self):
+        self.model = Model()
+        await self.model.connect(model_name=MODEL_NAME)
 
-    await asyncio.gather(
-        ops_test.model.deploy(
-            charm,
-            application_name=APP_NAME,
-            config={
-                "access-interface-name": "enp6s0",
-                "core-interface-name": "enp7s0",
-            },
-            to=0,
-        ),
-        ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000),
-    )
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_built_when_deploy_than_charm_goes_to_active_status(
+        self, ops_test: OpsTest
+    ):
+        charm = await ops_test.build_charm(".")
+        await ops_test.model.connect(model_name=MODEL_NAME)
 
+        await asyncio.gather(
+            ops_test.model.deploy(
+                charm,
+                application_name=APP_NAME,
+                config={
+                    "access-interface-name": ACCESS_INTERFACE_NAME,
+                    "core-interface-name": CORE_INTERFACE_NAME,
+                },
+                to=0,
+            ),
+            ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000),
+        )
 
-@pytest.mark.abort_on_fail
-async def test_given_upf_machine_charm_is_deployed_when_inspect_upf_config_file_then_access_and_core_interfaces_match_charm_config():  # noqa: E501
-    machine = await _get_machine("0")
-    application = await _get_application(APP_NAME)
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_inspect_upf_config_file_then_access_and_core_interfaces_match_charm_config(  # noqa: E501
+        self,
+    ):
+        machine = await self._get_machine("0")
+        application = await self._get_application(APP_NAME)
 
-    upf_config = await _get_upf_config(machine)
-    charm_config = await application.get_config()
+        upf_config = await self._get_upf_config(machine)
+        charm_config = await application.get_config()
 
-    assert upf_config["access"]["ifname"] == charm_config["access-interface-name"]["value"]
-    assert upf_config["core"]["ifname"] == charm_config["core-interface-name"]["value"]
+        assert upf_config["access"]["ifname"] == charm_config["access-interface-name"]["value"]
+        assert upf_config["core"]["ifname"] == charm_config["core-interface-name"]["value"]
 
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_inspect_upf_config_file_then_ip_masquerade_config_matches_core_iface_ip(  # noqa: E501
+        self,
+    ):
+        machine = await self._get_machine("0")
+        application = await self._get_application(APP_NAME)
 
-@pytest.mark.abort_on_fail
-async def test_given_upf_machine_charm_is_deployed_when_inspect_upf_config_file_then_ip_masquerade_config_matches_core_iface_ip():  # noqa: E501
-    machine = await _get_machine("0")
-    application = await _get_application(APP_NAME)
+        upf_config = await self._get_upf_config(machine)
+        charm_config = await application.get_config()
+        core_interface_name = charm_config["core-interface-name"]["value"]
 
-    upf_config = await _get_upf_config(machine)
-    charm_config = await application.get_config()
-    core_interface_name = charm_config["core-interface-name"]["value"]
+        core_iface_info = await machine.ssh(
+            f"ip -f inet addr show {core_interface_name} | grep inet"
+        )
+        core_iface_ip = core_iface_info.split()[1].split("/")[0]
 
-    core_iface_info = await machine.ssh(f"ip -f inet addr show {core_interface_name} | grep inet")
-    core_iface_ip = core_iface_info.split()[1].split("/")[0]
+        assert upf_config["core"]["ip_masquerade"] == core_iface_ip
 
-    assert upf_config["core"]["ip_masquerade"] == core_iface_ip
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_inspect_upf_config_file_then_dnn_matches_charm_config(  # noqa: E501
+        self,
+    ):
+        machine = await self._get_machine("0")
+        application = await self._get_application(APP_NAME)
 
+        upf_config = await self._get_upf_config(machine)
+        charm_config = await application.get_config()
 
-@pytest.mark.abort_on_fail
-async def test_given_upf_machine_charm_is_deployed_when_inspect_upf_config_file_then_dnn_matches_charm_config():  # noqa: E501
-    machine = await _get_machine("0")
-    application = await _get_application(APP_NAME)
+        assert upf_config["cpiface"]["dnn"] == charm_config["dnn"]["value"]
 
-    upf_config = await _get_upf_config(machine)
-    charm_config = await application.get_config()
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_list_processes_then_upf_processes_are_running(  # noqa: E501
+        self,
+    ):
+        machine = await self._get_machine("0")
+        application = await self._get_application(APP_NAME)
 
-    assert upf_config["cpiface"]["dnn"] == charm_config["dnn"]["value"]
+        charm_config = await application.get_config()
+        access_interface_name = charm_config["access-interface-name"]["value"]
+        core_interface_name = charm_config["core-interface-name"]["value"]
 
+        processes = await machine.ssh("ps aux | grep /snap/sdcore-upf")
 
-@pytest.mark.abort_on_fail
-async def test_given_upf_machine_charm_is_deployed_when_list_processes_then_upf_processes_are_running():  # noqa: E501
-    machine = await _get_machine("0")
-    application = await _get_application(APP_NAME)
+        assert "bessd -f -grpc-url=0.0.0.0:10514 -m 0" in processes
+        assert f"route_control.py -i {access_interface_name} {core_interface_name}" in processes
+        assert (
+            f"pfcpiface -config {os.path.join(UPF_CONFIG_PATH, UPF_CONFIG_FILE_NAME)}" in processes
+        )
 
-    charm_config = await application.get_config()
-    access_interface_name = charm_config["access-interface-name"]["value"]
-    core_interface_name = charm_config["core-interface-name"]["value"]
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_check_open_ports_then_pfcp_port_is_listening(  # noqa: E501
+        self,
+    ):
+        machine = await self._get_machine("0")
 
-    processes = await machine.ssh("ps aux | grep /snap/sdcore-upf")
+        open_ports = await machine.ssh("netstat -ulnp")
 
-    assert "bessd -f -grpc-url=0.0.0.0:10514 -m 0" in processes
-    assert f"route_control.py -i {access_interface_name} {core_interface_name}" in processes
-    assert f"pfcpiface -config {UPF_CONFIG_FILE_PATH}" in processes
+        assert str(PFCP_PORT) in open_ports
 
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_check_bessd_logs_then_no_errors_in_the_logs(  # noqa: E501
+        self,
+    ):
+        machine = await self._get_machine("0")
 
-@pytest.mark.abort_on_fail
-async def test_given_upf_machine_charm_is_deployed_when_check_open_ports_then_pfcp_port_is_listening():  # noqa: E501
-    machine = await _get_machine("0")
+        bessd_logs = await machine.ssh("journalctl | grep sdcore-upf.bessd | grep -v audit")
 
-    open_ports = await machine.ssh("netstat -ulnp")
+        assert not re.search("error", bessd_logs, re.IGNORECASE)
+        assert not re.search("traceback", bessd_logs, re.IGNORECASE)
 
-    assert str(PFCP_PORT) in open_ports
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_check_routes_then_route_to_gnb_subnet_is_set(  # noqa: E501
+        self,
+    ):
+        machine = await self._get_machine("0")
+        application = await self._get_application(APP_NAME)
 
+        charm_config = await application.get_config()
+        gnb_subnet = charm_config["gnb-subnet"]["value"]
+        access_interface_name = charm_config["access-interface-name"]["value"]
+        routes = await machine.ssh("ip route")
+        gnb_route_pattern = "^%s via \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} dev %s proto static" % (  # noqa: E501, W605
+            gnb_subnet, access_interface_name
+        )
 
-@pytest.mark.abort_on_fail
-async def test_given_upf_machine_charm_is_deployed_when_check_bessd_logs_then_no_errors_in_the_logs():  # noqa: E501
-    machine = await _get_machine("0")
+        assert any([re.match(gnb_route_pattern, route) for route in routes.splitlines()])
 
-    bessd_logs = await machine.ssh("journalctl | grep sdcore-upf.bessd | grep -v audit")
-    logger.error(bessd_logs)
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_check_routes_then_default_route_for_core_interface_is_set(  # noqa: E501
+        self,
+    ):
+        machine = await self._get_machine("0")
+        application = await self._get_application(APP_NAME)
 
-    assert not re.search("error", bessd_logs, re.IGNORECASE)
-    assert not re.search("traceback", bessd_logs, re.IGNORECASE)
+        charm_config = await application.get_config()
+        core_interface_name = charm_config["core-interface-name"]["value"]
+        routes = await machine.ssh("ip route")
+        core_default_route_pattern = "^default via \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} dev %s proto static metric 110" % (  # noqa: E501
+            core_interface_name,
+        )
 
+        assert any([re.match(core_default_route_pattern, route) for route in routes.splitlines()])
 
-async def _get_machine(machine_id: str):
-    model = Model()
-    await model.connect(model_name=MODEL_NAME)
-    return Machine(entity_id=machine_id, model=model)
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_charm_config_changed_then_upf_config_file_is_updated(  # noqa: E501
+        self,
+    ):
+        test_dnn_name = "integration"
+        machine = await self._get_machine("0")
+        application = await self._get_application(APP_NAME)
 
+        await application.set_config(config={"dnn": test_dnn_name})
+        await self.model.wait_for_idle(status="active")
+        upf_config = await self._get_upf_config(machine)
 
-async def _get_application(application_name: str):
-    model = Model()
-    await model.connect(model_name=MODEL_NAME)
-    return Application(entity_id=application_name, model=model)
+        assert upf_config["cpiface"]["dnn"] == test_dnn_name
 
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_when_charm_config_changed_to_invalid_then_charm_goes_to_blocked_state(  # noqa: E501
+        self,
+    ):
+        invalid_access_iface_name = "whatever"
+        application = await self._get_application(APP_NAME)
 
-async def _get_upf_config(machine):
-    await machine.scp_from(UPF_CONFIG_FILE_PATH, ".")
-    with open(UPF_CONFIG_FILE_PATH.split("/")[-1], "r") as upf_config_file:
-        upf_config = json.loads(upf_config_file.read())
-    return upf_config
+        await application.set_config(config={"access-interface-name": invalid_access_iface_name})
+        await self.model.wait_for_idle()
+
+        assert application.status == "blocked"
+
+    @pytest.mark.abort_on_fail
+    async def test_given_upf_machine_charm_is_deployed_and_in_blocked_status_when_charm_config_changed_back_to_valid_then_charm_goes_to_active_state(  # noqa: E501
+        self,
+    ):
+        application = await self._get_application(APP_NAME)
+
+        await application.set_config(config={"access-interface-name": ACCESS_INTERFACE_NAME})
+        await self.model.wait_for_idle()
+
+        assert application.status == "active"
+
+    async def _get_machine(self, machine_id: str):
+        return Machine(entity_id=machine_id, model=self.model)
+
+    async def _get_application(self, application_name: str):
+        return Application(entity_id=application_name, model=self.model)
+
+    @staticmethod
+    async def _get_upf_config(machine):
+        await machine.scp_from(os.path.join(UPF_CONFIG_PATH, UPF_CONFIG_FILE_NAME), ".")
+        with open(UPF_CONFIG_FILE_NAME, "r") as upf_config_file:
+            upf_config = json.loads(upf_config_file.read())
+        return upf_config
