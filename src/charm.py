f93caaf45d93258c49915becc9c6a6bd1f3a9089
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2024-07-11 20:18:56 +0200

    Adds back the snap revision

--- src/charm.py
+++ src/charm.py
@@ -27,6 +27,7 @@ from upf_network import UPFNetwork
 
 UPF_SNAP_NAME = "sdcore-upf"
 UPF_SNAP_CHANNEL = "1.4/edge"
+UPF_SNAP_REVISION = "54"
 UPF_CONFIG_FILE_NAME = "upf.json"
 UPF_CONFIG_PATH = "/var/snap/sdcore-upf/common"
 PFCP_PORT = 8805
@@ -221,7 +222,7 @@ class SdcoreUpfCharm(ops.CharmBase):
         """Check if the UPF snap is installed."""
         snap_cache = SnapCache()
         upf_snap = snap_cache[UPF_SNAP_NAME]
-        return upf_snap.state == SnapState.Latest
+        return upf_snap.state == SnapState.Latest and upf_snap.revision == UPF_SNAP_REVISION
 
     def _start_bessd_service(self) -> None:
         """Start the bessd service."""
